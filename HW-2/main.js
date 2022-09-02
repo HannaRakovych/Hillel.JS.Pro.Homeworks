a769bcc6ef0de80adde4c8217b572e72b28158ba
Anna Rakovych <anna.rakovych96@gmail.com>
2022-09-02 14:11:19 +0300

    Add second homework

---
+++ HW-2/main.js
@@ -0,0 +1,39 @@
+const myName = "Anna";
+console.log(myName);
+
+let mySurname = "Rakovych";
+console.log(mySurname);
+
+const myCity = "Kyiv";
+console.log(myCity);
+
+let average = (11 + 22 + 33 + 44) / 4;
+console.log(average);
+
+let circleRadius = 14;
+console.log(circleRadius);
+
+// ввожу змінну зі значенням числа Пі //
+let numberP = 3.14;
+
+let length;
+let square;
+
+length = 2 * circleRadius * numberP;
+console.log(length);
+square = numberP * circleRadius ** 2;
+console.log(square);
+
+/* створюю невірні змінні
+
+let 5redrabbits;
+
+let color-red;
+
+let @totalSum;
+
+*/
+
+
+
+
